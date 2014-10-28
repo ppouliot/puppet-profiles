6fbf33e84c24a5e4149171cc65ceeb1f3d884b13
Peter J. Pouliot <peter@pouliot.net>
2014-10-27 22:08:37 -0400

    committing bits

---
+++ manifests/puppet_developer.pp
@@ -0,0 +1,8 @@
+class profiles::puppet_developer {
+  case $osfamily {
+    "Debian":{}
+    "Redhat":{}
+    "Windows":{}
+    "FreeBSD":{}
+    default:{}
+}
--- manifests/user_account.pp
+++ manifests/user_account.pp
@@ -1,6 +1,12 @@
 # Defined type for creating virtual user accounts
 #
-define profiless::user_account ($uid,$realname,$pass) {
+#
+#  @profiles::user_account {'ovswitch':
+#    uid => 500,
+#    realname => 'OpenVSwitch Account',
+#  }
+#
+define profiless::user_account ($uid,$realname,$pass,$homedir) {
  
   user { $title:
     ensure            =>  'present',
@@ -17,8 +23,10 @@ define profiless::user_account ($uid,$realname,$pass) {
   group { $title:
     gid               =>  $uid,
   }
- 
-  file { "/home/${title}":
+  if $homedir == undef { 
+     $homedir = "/home/${title}"
+  }
+  file { $homedir:
     ensure            =>  directory,
     owner             =>  $title,
     group             =>  $title,
