52b0da6ac92483ae1cf1297b8a9e51dbfe4e1ef8
Peter J. Pouliot <ppouliot@gmail.com>
2014-12-31 15:38:09 +0000

    fixing hp.pp to make it a proper class

--- manifests/hp.pp
+++ manifests/hp.pp
@@ -1,21 +1,24 @@
-case $manufacturer {
-  $msg_product = "${hostname} is a ${manufacturer} ${productname}"
-  'HP':{
-    case $productname {
-      'ProLiant DL140 G3':{
-        warn($msg_product)
+class profiles::hp {  
+
+  case $manufacturer {
+    $msg_product = "${hostname} is a ${manufacturer} ${productname}"
+    'HP':{
+      case $productname {
+        'ProLiant DL140 G3':{
+          warn($msg_product)
+        }
+        'ProLiant SE326M1':{
+          warn($msg_product)
+        }
+        'ProLiant SE316M1R2':{
+          warn($msg_product)
+        }
+        default:{
+          warn("this is an unknown HP product ${productname} in our infrastructure")
+        }
       }
-      'ProLiant SE326M1':{
-        warn($msg_product)
-      }
-      'ProLiant SE316M1R2':{
-        warn($msg_product)
-      }
-      default:{
-        warn("this is an unknown HP product ${productname} in our infrastructure")
-      }
+    default:{
+      warn("!!! Manufacturer: ${manufacturer} is not HP")
     }
-  default:{
-    warn("!!! Manufacturer: ${manufacturer} is not HP")
   }
 }
