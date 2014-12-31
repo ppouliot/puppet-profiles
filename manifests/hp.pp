6777aa248af87f8a189782047508dd85351bb41a
Peter J. Pouliot <ppouliot@gmail.com>
2014-12-31 15:48:15 +0000

    hp updates

--- manifests/hp.pp
+++ manifests/hp.pp
@@ -1,24 +1,31 @@
 class profiles::hp {  
-
-  case $manufacturer {
-    $msg_product = "${hostname} is a ${manufacturer} ${productname}"
-    'HP':{
-      case $productname {
-        'ProLiant DL140 G3':{
-          warn($msg_product)
-        }
-        'ProLiant SE326M1':{
-          warn($msg_product)
-        }
-        'ProLiant SE316M1R2':{
-          warn($msg_product)
-        }
-        default:{
-          warn("this is an unknown HP product ${productname} in our infrastructure")
+  $msg_product = "${hostname} is a ${manufacturer} ${productname}"
+  case $virtual {
+    'physical':{
+      case $manufacturer {
+        'HP':{
+          case $productname {
+            'ProLiant DL140 G3':{
+              warn($msg_product)
+            }
+            'ProLiant SE326M1':{
+              warn($msg_product)
+            }
+            'ProLiant SE316M1R2':{
+              warn($msg_product)
+            }
+            default:{
+              warn("this is an unknown HP product ${productname} in our infrastructure")
+            }
+          }
+          default:{
+            warn("!!! Manufacturer: ${manufacturer} is not HP")
+          }
         }
       }
-    default:{
-      warn("!!! Manufacturer: ${manufacturer} is not HP")
+      default:{
+            warn("!!! Virtual is set to ${virtual} and must be physical")
+      }
     }
   }
 }
