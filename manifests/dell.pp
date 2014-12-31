3fbdec553dd744b1fd05817ca3ad165637e8a690
Peter J. Pouliot <ppouliot@gmail.com>
2014-12-31 15:36:13 +0000

    updating dell hardware types

--- manifests/dell.pp
+++ manifests/dell.pp
@@ -1,61 +1,66 @@
-case $manufacturer {
-  'Dell Inc.':{
-#    include dell_openmanage
-#    include basenode::ipmitools
-
-    $msg_product = "${hostname} is a ${manufacturer} ${productname}"
-    if $ipaddress =~ /([001-254\.])/{ 
-      $octet1 => $1
-      $octet2 => $2
-      $octet3 => $3
-      $octet4 => $4
+class profiles::dell {
+  case $manufacturer {
+    'Dell Inc.':{
+      $msg_product = "${hostname} is a ${manufacturer} ${productname}"
+      case $productname {
+        'PowerEdge M600': {
+          warn($msg_product)
+         # Insert M600 Classes or logic here
+        }
+  
+        'PowerEdge M605': {
+          warn($msg_product)
+         # Insert M605 Classes or logic here
+        }
+  
+        'PowerEdge M610': {
+          warn($msg_product)
+         # Insert M610 Classes here
+        }
+  
+        'PowerEdge 860': {
+          warn($msg_product)
+         # Insert 860 Classes here
+        }
+  
+        'PowerEdge 1950': {
+          warn($msg_product)
+         # Insert 1950 Classes here
+        }
+  
+        'PowerEdge 2850': {
+          warn($msg_product)
+         # Insert 2850 Classes here
+        }
+  
+        'PowerEdge 2950': {
+          warn($msg_product)
+         # Insert 2950 Classes here
+        }
+  
+        'PowerEdge R200': {
+          warn($msg_product)
+         # Insert R200 Classes here
+        }
+  
+        'PowerEdge R610': {
+          warn($msg_product)
+         # Insert R610 Classes here
+        }
+  
+        'PowerEdge R710': {
+          warn($msg_product)
+         # Insert R710 Classes here
+        }
+  
+        default:{
+          warn("this is an unknown Dell product ${productname}")
+        }
+      }
+  
+    default:{
+      warn("!!! Manufacturer: ${manufacturer} is not Dell Inc. !!!")
     }
-
-#    $default_ipmi_ipaddress = "$10.99.99.${octet4}",
-#    exec {'ipmi-tool_dump_mac_addresses':
-#      command   => 'ipmitool -H  $default_ipmi_address -U $ipmi_user -P ipmi_password delloem mac |grep [0-9]:',
-#      logoutput => true
-#      require   => Package['ipmi-tool'],
-#    }
-    case $productname {
-
-      'PowerEdge M600': {
-        warn($msg_product)
-       # Insert M600 Classes or logic here
-
-      }
-
-      'PowerEdge M605': {
-        warn($msg_product)
-       # Insert M605 Classes or logic here
-
-      }
-
-      'PowerEdge M610': {
-        warn($msg_product)
-        notify {$msg_product:}
-       # Insert M610 Classes here
-
-      }
-      'PowerEdge 2950': {
-        warn($msg_product)
-        notify {$msg_product:}
-       # Insert 2950 Classes here
-      }
-      'PowerEdge R200': {
-        warn($msg_product)
-        notify {$msg_product:}
-       # Insert R200 Classes here
-      }
-      'PowerEdge R710': {
-        warn($msg_product)
-       # Insert R710 Classes here
-      }
-      default:{
-        warn("this is an unknown Dell product ${productname}")
-      }
-    }
-  default:{
-    warn("!!! Manufacturer: ${manufacturer} is not Dell Inc. !!!")
+  
   }
 }
