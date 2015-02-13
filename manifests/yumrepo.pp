4b1d57a03410efe3e6de73d3aabe49f6a7ce97e3
Peter J. Pouliot <ppouliot@gmail.com>
2015-02-13 15:12:53 -0500

    puppet resource yumrepo from production kvm node

---
+++ manifests/yumrepo.pp
@@ -0,0 +1,379 @@
+yumrepo { 'C6.0-base':
+  baseurl  => 'http://vault.centos.org/6.0/os/$basearch/',
+  descr    => 'CentOS-6.0 - Base',
+  enabled  => '0',
+  gpgcheck => '1',
+  gpgkey   => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-6',
+}
+yumrepo { 'C6.0-centosplus':
+  baseurl  => 'http://vault.centos.org/6.0/centosplus/$basearch/',
+  descr    => 'CentOS-6.0 - CentOSPlus',
+  enabled  => '0',
+  gpgcheck => '1',
+  gpgkey   => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-6',
+}
+yumrepo { 'C6.0-contrib':
+  baseurl  => 'http://vault.centos.org/6.0/contrib/$basearch/',
+  descr    => 'CentOS-6.0 - Contrib',
+  enabled  => '0',
+  gpgcheck => '1',
+  gpgkey   => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-6',
+}
+yumrepo { 'C6.0-extras':
+  baseurl  => 'http://vault.centos.org/6.0/extras/$basearch/',
+  descr    => 'CentOS-6.0 - Extras',
+  enabled  => '0',
+  gpgcheck => '1',
+  gpgkey   => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-6',
+}
+yumrepo { 'C6.0-updates':
+  baseurl  => 'http://vault.centos.org/6.0/updates/$basearch/',
+  descr    => 'CentOS-6.0 - Updates',
+  enabled  => '0',
+  gpgcheck => '1',
+  gpgkey   => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-6',
+}
+yumrepo { 'C6.1-base':
+  baseurl  => 'http://vault.centos.org/6.1/os/$basearch/',
+  descr    => 'CentOS-6.1 - Base',
+  enabled  => '0',
+  gpgcheck => '1',
+  gpgkey   => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-6',
+}
+yumrepo { 'C6.1-centosplus':
+  baseurl  => 'http://vault.centos.org/6.1/centosplus/$basearch/',
+  descr    => 'CentOS-6.1 - CentOSPlus',
+  enabled  => '0',
+  gpgcheck => '1',
+  gpgkey   => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-6',
+}
+yumrepo { 'C6.1-contrib':
+  baseurl  => 'http://vault.centos.org/6.1/contrib/$basearch/',
+  descr    => 'CentOS-6.1 - Contrib',
+  enabled  => '0',
+  gpgcheck => '1',
+  gpgkey   => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-6',
+}
+yumrepo { 'C6.1-extras':
+  baseurl  => 'http://vault.centos.org/6.1/extras/$basearch/',
+  descr    => 'CentOS-6.1 - Extras',
+  enabled  => '0',
+  gpgcheck => '1',
+  gpgkey   => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-6',
+}
+yumrepo { 'C6.1-updates':
+  baseurl  => 'http://vault.centos.org/6.1/updates/$basearch/',
+  descr    => 'CentOS-6.1 - Updates',
+  enabled  => '0',
+  gpgcheck => '1',
+  gpgkey   => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-6',
+}
+yumrepo { 'C6.2-base':
+  baseurl  => 'http://vault.centos.org/6.2/os/$basearch/',
+  descr    => 'CentOS-6.2 - Base',
+  enabled  => '0',
+  gpgcheck => '1',
+  gpgkey   => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-6',
+}
+yumrepo { 'C6.2-centosplus':
+  baseurl  => 'http://vault.centos.org/6.2/centosplus/$basearch/',
+  descr    => 'CentOS-6.2 - CentOSPlus',
+  enabled  => '0',
+  gpgcheck => '1',
+  gpgkey   => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-6',
+}
+yumrepo { 'C6.2-contrib':
+  baseurl  => 'http://vault.centos.org/6.2/contrib/$basearch/',
+  descr    => 'CentOS-6.2 - Contrib',
+  enabled  => '0',
+  gpgcheck => '1',
+  gpgkey   => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-6',
+}
+yumrepo { 'C6.2-extras':
+  baseurl  => 'http://vault.centos.org/6.2/extras/$basearch/',
+  descr    => 'CentOS-6.2 - Extras',
+  enabled  => '0',
+  gpgcheck => '1',
+  gpgkey   => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-6',
+}
+yumrepo { 'C6.2-updates':
+  baseurl  => 'http://vault.centos.org/6.2/updates/$basearch/',
+  descr    => 'CentOS-6.2 - Updates',
+  enabled  => '0',
+  gpgcheck => '1',
+  gpgkey   => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-6',
+}
+yumrepo { 'C6.3-base':
+  baseurl  => 'http://vault.centos.org/6.3/os/$basearch/',
+  descr    => 'CentOS-6.3 - Base',
+  enabled  => '0',
+  gpgcheck => '1',
+  gpgkey   => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-6',
+}
+yumrepo { 'C6.3-centosplus':
+  baseurl  => 'http://vault.centos.org/6.3/centosplus/$basearch/',
+  descr    => 'CentOS-6.3 - CentOSPlus',
+  enabled  => '0',
+  gpgcheck => '1',
+  gpgkey   => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-6',
+}
+yumrepo { 'C6.3-contrib':
+  baseurl  => 'http://vault.centos.org/6.3/contrib/$basearch/',
+  descr    => 'CentOS-6.3 - Contrib',
+  enabled  => '0',
+  gpgcheck => '1',
+  gpgkey   => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-6',
+}
+yumrepo { 'C6.3-extras':
+  baseurl  => 'http://vault.centos.org/6.3/extras/$basearch/',
+  descr    => 'CentOS-6.3 - Extras',
+  enabled  => '0',
+  gpgcheck => '1',
+  gpgkey   => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-6',
+}
+yumrepo { 'C6.3-updates':
+  baseurl  => 'http://vault.centos.org/6.3/updates/$basearch/',
+  descr    => 'CentOS-6.3 - Updates',
+  enabled  => '0',
+  gpgcheck => '1',
+  gpgkey   => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-6',
+}
+yumrepo { 'C6.4-base':
+  baseurl  => 'http://vault.centos.org/6.4/os/$basearch/',
+  descr    => 'CentOS-6.4 - Base',
+  enabled  => '0',
+  gpgcheck => '1',
+  gpgkey   => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-6',
+}
+yumrepo { 'C6.4-centosplus':
+  baseurl  => 'http://vault.centos.org/6.4/centosplus/$basearch/',
+  descr    => 'CentOS-6.4 - CentOSPlus',
+  enabled  => '0',
+  gpgcheck => '1',
+  gpgkey   => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-6',
+}
+yumrepo { 'C6.4-contrib':
+  baseurl  => 'http://vault.centos.org/6.4/contrib/$basearch/',
+  descr    => 'CentOS-6.4 - Contrib',
+  enabled  => '0',
+  gpgcheck => '1',
+  gpgkey   => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-6',
+}
+yumrepo { 'C6.4-extras':
+  baseurl  => 'http://vault.centos.org/6.4/extras/$basearch/',
+  descr    => 'CentOS-6.4 - Extras',
+  enabled  => '0',
+  gpgcheck => '1',
+  gpgkey   => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-6',
+}
+yumrepo { 'C6.4-updates':
+  baseurl  => 'http://vault.centos.org/6.4/updates/$basearch/',
+  descr    => 'CentOS-6.4 - Updates',
+  enabled  => '0',
+  gpgcheck => '1',
+  gpgkey   => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-6',
+}
+yumrepo { 'Xen4CentOS':
+  baseurl  => 'http://mirror.centos.org/centos/$releasever/xen4/$basearch/',
+  descr    => 'CentOS-$releasever - xen',
+  enabled  => '1',
+  gpgcheck => '1',
+  gpgkey   => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-6',
+}
+yumrepo { 'base':
+  descr      => 'CentOS-$releasever - Base',
+  gpgcheck   => '1',
+  gpgkey     => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-6',
+  mirrorlist => 'http://mirrorlist.centos.org/?release=$releasever&arch=$basearch&repo=os',
+}
+yumrepo { 'c6-media':
+  baseurl  => 'file:///media/CentOS/
+        file:///media/cdrom/
+        file:///media/cdrecorder/',
+  descr    => 'CentOS-$releasever - Media',
+  enabled  => '0',
+  gpgcheck => '1',
+  gpgkey   => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-6',
+}
+yumrepo { 'centosplus':
+  descr      => 'CentOS-$releasever - Plus',
+  enabled    => '0',
+  gpgcheck   => '1',
+  gpgkey     => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-6',
+  mirrorlist => 'http://mirrorlist.centos.org/?release=$releasever&arch=$basearch&repo=centosplus',
+}
+yumrepo { 'contrib':
+  descr      => 'CentOS-$releasever - Contrib',
+  enabled    => '0',
+  gpgcheck   => '1',
+  gpgkey     => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-6',
+  mirrorlist => 'http://mirrorlist.centos.org/?release=$releasever&arch=$basearch&repo=contrib',
+}
+yumrepo { 'debug':
+  baseurl  => 'http://debuginfo.centos.org/6/$basearch/',
+  descr    => 'CentOS-6 - Debuginfo',
+  enabled  => '0',
+  gpgcheck => '1',
+  gpgkey   => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-Debug-6',
+}
+yumrepo { 'dell-omsa-indep':
+  descr      => 'Dell OMSA repository - Hardware independent',
+  enabled    => '1',
+  gpgcheck   => '1',
+  gpgkey     => 'http://linux.dell.com/repo/hardware/latest/RPM-GPG-KEY-dell
+    http://linux.dell.com/repo/hardware/latest/RPM-GPG-KEY-libsmbios',
+  mirrorlist => 'http://linux.dell.com/repo/hardware/latest/mirrors.cgi?osname=el$releasever&basearch=$basearch&native=1&dellsysidpluginver=$dellsysidpluginver',
+}
+yumrepo { 'dell-omsa-specific':
+  descr      => 'Dell OMSA repository - Hardware specific',
+  enabled    => '1',
+  gpgcheck   => '1',
+  gpgkey     => 'http://linux.dell.com/repo/hardware/latest/RPM-GPG-KEY-dell
+    http://linux.dell.com/repo/hardware/latest/RPM-GPG-KEY-libsmbios',
+  mirrorlist => 'http://linux.dell.com/repo/hardware/latest/mirrors.cgi?osname=el$releasever&basearch=$basearch&native=1&sys_ven_id=$sys_ven_id&sys_dev_id=$sys_dev_id&dellsysidpluginver=$dellsysidpluginver',
+}
+yumrepo { 'epel':
+  descr          => 'Extra Packages for Enterprise Linux 6 - $basearch',
+  enabled        => '1',
+  failovermethod => 'priority',
+  gpgcheck       => '1',
+  gpgkey         => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-EPEL-6',
+  mirrorlist     => 'https://mirrors.fedoraproject.org/metalink?repo=epel-6&arch=$basearch',
+}
+yumrepo { 'epel-debuginfo':
+  descr          => 'Extra Packages for Enterprise Linux 6 - $basearch - Debug',
+  enabled        => '0',
+  failovermethod => 'priority',
+  gpgcheck       => '1',
+  gpgkey         => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-EPEL-6',
+  mirrorlist     => 'https://mirrors.fedoraproject.org/metalink?repo=epel-debug-6&arch=$basearch',
+}
+yumrepo { 'epel-source':
+  descr          => 'Extra Packages for Enterprise Linux 6 - $basearch - Source',
+  enabled        => '0',
+  failovermethod => 'priority',
+  gpgcheck       => '1',
+  gpgkey         => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-EPEL-6',
+  mirrorlist     => 'https://mirrors.fedoraproject.org/metalink?repo=epel-source-6&arch=$basearch',
+}
+yumrepo { 'epel-testing':
+  descr          => 'Extra Packages for Enterprise Linux 6 - Testing - $basearch',
+  enabled        => '0',
+  failovermethod => 'priority',
+  gpgcheck       => '1',
+  gpgkey         => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-EPEL-6',
+  mirrorlist     => 'https://mirrors.fedoraproject.org/metalink?repo=testing-epel6&arch=$basearch',
+}
+yumrepo { 'epel-testing-debuginfo':
+  descr          => 'Extra Packages for Enterprise Linux 6 - Testing - $basearch - Debug',
+  enabled        => '0',
+  failovermethod => 'priority',
+  gpgcheck       => '1',
+  gpgkey         => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-EPEL-6',
+  mirrorlist     => 'https://mirrors.fedoraproject.org/metalink?repo=testing-debug-epel6&arch=$basearch',
+}
+yumrepo { 'epel-testing-source':
+  descr          => 'Extra Packages for Enterprise Linux 6 - Testing - $basearch - Source',
+  enabled        => '0',
+  failovermethod => 'priority',
+  gpgcheck       => '1',
+  gpgkey         => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-EPEL-6',
+  mirrorlist     => 'https://mirrors.fedoraproject.org/metalink?repo=testing-source-epel6&arch=$basearch',
+}
+yumrepo { 'extras':
+  descr      => 'CentOS-$releasever - Extras',
+  gpgcheck   => '1',
+  gpgkey     => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-6',
+  mirrorlist => 'http://mirrorlist.centos.org/?release=$releasever&arch=$basearch&repo=extras',
+}
+yumrepo { 'foreman':
+  baseurl  => 'http://yum.theforeman.org/releases/1.3/el6/$basearch',
+  descr    => 'Foreman stable',
+  enabled  => '1',
+  gpgcheck => '1',
+  gpgkey   => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-foreman',
+}
+yumrepo { 'foreman-plugins':
+  baseurl  => 'http://yum.theforeman.org/plugins/1.3/el6/$basearch',
+  descr    => 'Foreman stable - plugins',
+  enabled  => '1',
+  gpgcheck => '0',
+  gpgkey   => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-foreman',
+}
+yumrepo { 'foreman-plugins-source':
+  baseurl  => 'http://yum.theforeman.org/plugins/1.3/el6/source',
+  descr    => 'Foreman stable - plugins source',
+  enabled  => '0',
+  gpgcheck => '0',
+  gpgkey   => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-foreman',
+}
+yumrepo { 'foreman-source':
+  baseurl  => 'http://yum.theforeman.org/releases/1.3/el6/source',
+  descr    => 'Foreman stable - source',
+  enabled  => '0',
+  gpgcheck => '1',
+  gpgkey   => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-foreman',
+}
+yumrepo { 'openstack-havana':
+  baseurl  => 'http://repos.fedorapeople.org/repos/openstack/openstack-havana/epel-6/',
+  descr    => 'OpenStack Havana Repository',
+  enabled  => '1',
+  gpgcheck => '1',
+  gpgkey   => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-RDO-Havana',
+  priority => '98',
+}
+yumrepo { 'puppetlabs-deps':
+  baseurl  => 'http://yum.puppetlabs.com/el/6/dependencies/$basearch',
+  descr    => 'Puppet Labs Dependencies El 6 - $basearch',
+  enabled  => '1',
+  gpgcheck => '1',
+  gpgkey   => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-puppetlabs',
+}
+yumrepo { 'puppetlabs-deps-source':
+  baseurl  => 'http://yum.puppetlabs.com/el/6/dependencies/SRPMS',
+  descr    => 'Puppet Labs Source Dependencies El 6 - $basearch - Source',
+  enabled  => '0',
+  gpgcheck => '1',
+  gpgkey   => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-puppetlabs',
+}
+yumrepo { 'puppetlabs-devel':
+  baseurl  => 'http://yum.puppetlabs.com/el/6/devel/$basearch',
+  descr    => 'Puppet Labs Devel El 6 - $basearch',
+  enabled  => '0',
+  gpgcheck => '1',
+  gpgkey   => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-puppetlabs',
+}
+yumrepo { 'puppetlabs-devel-source':
+  baseurl  => 'http://yum.puppetlabs.com/el/6/devel/SRPMS',
+  descr    => 'Puppet Labs Devel El 6 - $basearch - Source',
+  enabled  => '0',
+  gpgcheck => '1',
+  gpgkey   => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-puppetlabs',
+}
+yumrepo { 'puppetlabs-products':
+  baseurl  => 'http://yum.puppetlabs.com/el/6/products/$basearch',
+  descr    => 'Puppet Labs Products El 6 - $basearch',
+  enabled  => '1',
+  gpgcheck => '1',
+  gpgkey   => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-puppetlabs',
+}
+yumrepo { 'puppetlabs-products-source':
+  baseurl        => 'http://yum.puppetlabs.com/el/6/products/SRPMS',
+  descr          => 'Puppet Labs Products El 6 - $basearch - Source',
+  enabled        => '0',
+  failovermethod => 'priority',
+  gpgcheck       => '1',
+  gpgkey         => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-puppetlabs',
+}
+yumrepo { 'sensu':
+  baseurl  => 'http://repos.sensuapp.org/yum/el/$releasever/$basearch/',
+  descr    => 'sensu',
+  enabled  => '1',
+  gpgcheck => '0',
+}
+yumrepo { 'updates':
+  descr      => 'CentOS-$releasever - Updates',
+  gpgcheck   => '1',
+  gpgkey     => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-6',
+  mirrorlist => 'http://mirrorlist.centos.org/?release=$releasever&arch=$basearch&repo=updates',
+}
