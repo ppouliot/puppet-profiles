918635116d0f945b789dcaa920f6dbf31aa33c11
Peter J. Pouliot <ppouliot@gmail.com>
2015-04-08 12:19:13 -0400

    java

--- manifests/java_infra.pp
+++ manifests/java_infra.pp
@@ -1,10 +1,10 @@
-# = Class: profiles::java
+# = Class: profiles::java_infra
 #
 # A class to ensure remote access is enabled on a managed host
 # We will also bind to the $kernel fact and split the run between
 # windows and linux nodes
 #
-class profiles::java () {
+class profiles::java_infra () {
   validate_re($::kernel, '(^Linux|Windows)$', 'This Module only works on Linux and Windows Kernels.')
   $java_alert   = "Fullfilling ** JAVA ** site requirements based on host ${::fqdn} detection of ${::kernel} kernel"
   $java_failure = "FAILURE: ${::fqdn} failed to meet the requirements to operate in this site."
