a43646f0868668ada11ceb71d242bf219695829b
Peter J. Pouliot <ppouliot@gmail.com>
2015-04-10 16:20:58 -0400

    docker registry code

--- manifests/docker_registry.pp
+++ manifests/docker_registry.pp
@@ -2,21 +2,20 @@
 #
 # Instructions from here: 
 # http://blog.docker.io/2013/07/how-to-use-your-own-registry/
-#git clone https://github.com/dotcloud/docker-registry.git
+#git clone https://github.com/docker/docker-registry.git
 #cd docker-registry
 #cp config_sample.yml config.yml
 #pip install -r requirements.txt
 #gunicorn --access-logfile - --log-level debug --debug \
 #    -b 0.0.0.0:5000 -w 1 wsgi:application
-class profiles::docker_registry(
-)inherits docker::params {
+class profiles::docker_registry(){
 
   vcsrepo{'/usr/local/src/docker-registry':
     ensure   => present, 
     provider => git,
-    source   => 'https://github.com/dotcloud/docker-registry.git'
+    source   => 'https://github.com/docker/docker-registry.git'
   }
-  $docker_registry_prereq = ['python-openssl','libevent-dev','python-pip','python-dev']
+  $docker_registry_prereq = ['build-essential','python-openssl','libevent-dev','python-pip','python-dev']
 
   package { $docker_registry_prereq:
     ensure => latest,
@@ -29,12 +28,15 @@ class profiles::docker_registry(
   } 
 
   exec {'pip-install-requirements':
-    command   => '/usr/bin/pip install -r /usr/local/src/docker-registry/requirements.txt',
-#    command   => 'pip install -r requirements.txt',
-#    provider  => python,
-    cwd       => '/usr/local/src/docker-registry',
-    require   => [Package[$docker_registry_prereq],Vcsrepo['/usr/local/src/docker-registry']],
-    logoutput => true,
-    timeout   => 0,
+    command     => '/usr/bin/pip install -r /usr/local/src/docker-registry/requirements.txt',
+    cwd         => '/usr/local/src/docker-registry',
+    require     => [Package[$docker_registry_prereq],Vcsrepo['/usr/local/src/docker-registry']],
+    logoutput   => true,
+    refreshonly => true,
+    timeout     => 0,
+  }
+  package{'docker-registry':
+    provider => 'pip',
+    ensure   => installed,
   }
 } 
