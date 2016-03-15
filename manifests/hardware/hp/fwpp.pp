3d2c39e0886a689c57a5f9752fc1453dffdef82c
hypervci <hyper-v_ci@microsoft.com>
2016-03-15 23:32:24 +0000

    lint cleanup

--- manifests/hardware/hp/fwpp.pp
+++ manifests/hardware/hp/fwpp.pp
@@ -1,15 +1,14 @@
-class profiles::hp::fwpp {  
-
-  case $virtual {
+# == Class: profiles::hp::fwpp
+class profiles::hp::fwpp {
+  case $::virtual {
     'physical':{
       case $manufacturer {
         'HP':{
           case $osfamily:{
-          
         }
       }
       default:{
-        warning("!!! Virtual is set to ${virtual} and must be physical")
+        warning("!!! Virtual is set to ${::virtual} and must be physical")
       }
     }
   }
