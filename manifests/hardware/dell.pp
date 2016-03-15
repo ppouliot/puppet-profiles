cc7134a717691ba9d9ba9e3c1f1d0938cb6c944d
hypervci <hyper-v_ci@microsoft.com>
2016-03-15 23:19:39 +0000

    lint cleanup

--- manifests/hardware/dell.pp
+++ manifests/hardware/dell.pp
@@ -5,52 +5,52 @@ class profiles::dell {
       case $productname {
         'PowerEdge M600': {
           warn($msg_product)
-         # Insert M600 Classes or logic here
+          # Insert M600 Classes or logic here
         }
   
         'PowerEdge M605': {
           warn($msg_product)
-         # Insert M605 Classes or logic here
+          # Insert M605 Classes or logic here
         }
   
         'PowerEdge M610': {
           warn($msg_product)
-         # Insert M610 Classes here
+          # Insert M610 Classes here
         }
   
         'PowerEdge 860': {
           warn($msg_product)
-         # Insert 860 Classes here
+          # Insert 860 Classes here
         }
   
         'PowerEdge 1950': {
           warn($msg_product)
-         # Insert 1950 Classes here
+          # Insert 1950 Classes here
         }
   
         'PowerEdge 2850': {
           warn($msg_product)
-         # Insert 2850 Classes here
+          # Insert 2850 Classes here
         }
   
         'PowerEdge 2950': {
           warn($msg_product)
-         # Insert 2950 Classes here
+          # Insert 2950 Classes here
         }
   
         'PowerEdge R200': {
           warn($msg_product)
-         # Insert R200 Classes here
+          # Insert R200 Classes here
         }
   
         'PowerEdge R610': {
           warn($msg_product)
-         # Insert R610 Classes here
+          # Insert R610 Classes here
         }
   
         'PowerEdge R710': {
           warn($msg_product)
-         # Insert R710 Classes here
+          # Insert R710 Classes here
         }
   
         default:{
--- manifests/hardware/hp.pp
+++ manifests/hardware/hp.pp
@@ -1,51 +1,53 @@
-class profiles::hp {  
+# == Class: profiles::hp
+class profiles::hp {
 
-  $msg_product                   = "${fqdn} is a ${manufacturer} ${productname}"
-  $hyperv_minimimum_requirement  = "${fqdn} has ${memorysize} RAM and meets the minimum requirement for Hyper-V CI workloads"
-  $kvm_minimimum_requirement     = "${fqdn} has ${memorysize} RAM meets the minimum requirement for KVM CI workloads"
-  $kvm_insufficient_resources    = "${fqdn} has insufficient resources for KVM CI workloads"
-  $hyperv_insufficient_resources = "${fqdn} has insufficient resources for Hyper-V CI workloads"
+  $msg_product                   = "${::fqdn} is a ${::manufacturer} ${::productname}"
+  $hyperv_minimimum_requirement  = "${::fqdn} has ${::memorysize} RAM and meets the minimum requirement for Hyper-V CI workloads"
+  $kvm_minimimum_requirement     = "${::fqdn} has ${::memorysize} RAM meets the minimum requirement for KVM CI workloads"
+  $kvm_insufficient_resources    = "${::fqdn} has insufficient resources for KVM CI workloads"
+  $hyperv_insufficient_resources = "${::fqdn} has insufficient resources for Hyper-V CI workloads"
 
-  case $virtual {
+  case $::virtual {
     'physical':{
-      case $manufacturer {
+      case $::manufacturer {
         'HP':{
-          case $productname {
+          case $::productname {
 
             'ProLiant DL140 G3':{
               notice($msg_product)
-              warning("Default manufacturer IPMI User: admin Password: admin from ${manufacturer}")
+              warning("Default ::manufacturer IPMI User: admin Password: admin from ${::manufacturer}")
 
               #Basic memory size tests and notification
               ##
-              case $memorysize_mb {
+              case $::memorysize_mb {
                 '19988.54':{
-                  notice("$kvm_minimum_requirement")
-                  notice("$hyperv_minimum_requirement")
+                  notice($::kvm_minimum_requirement)
+                  notice($::hyperv_minimum_requirement)
                 }
                 '11674.64':{
-                  notice("$kvm_minimum_requirement")
-                  notice("$hyperv_minimum_requirement")
+                  notice($::kvm_minimum_requirement)
+                  notice($::hyperv_minimum_requirement)
                 }
                 '7442.41':{
-                  notice("$hyperv_minimum_requirement")
-                  warning("$kvm_insufficient_resources")
+                  notice($::hyperv_minimum_requirement)
+                  warning($::kvm_insufficient_resources)
                 }
 
                 '3829.62':{
-                  notice("$hyperv_minimum_requirement")
-                  warning("$kvm_insufficient_resources")
-                }  
-
+                  notice($::hyperv_minimum_requirement)
+                  warning($::kvm_insufficient_resources)
+                }
+                default:{ warning("not the appropriate memorysize ${::memorysize}")}
               }
+              default:{ warning("not the appropriate productname ${::productname}")}
             }
 
             'ProLiant SE326M1':{
-              notice($msg_product)
+              notice($::msg_product)
             }
 
             'ProLiant SE316M1R2':{
-              notice($msg_product)
+              notice($::msg_product)
             }
             'ProLiant DL360 G6':{
               notice($msg_product)
@@ -55,11 +57,11 @@ class profiles::hp {
               $hp_spp_iso_latest = 'spp2014020b'
             }
             default:{
-              warning("this is an unknown HP product ${productname} in our infrastructure")
-            } 
+              warning("this is an unknown HP product ${::productname} in our infrastructure")
+            }
           }
           default:{
-            warning("!!! Manufacturer: ${manufacturer} is not HP")
+            warning("!!! Manufacturer: ${::manufacturer} is not HP")
           }
 
         }
