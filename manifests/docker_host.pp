ba4e6b15ba00a313dd1266ab471063cd2b1da08d
primeministerp <primeministerpete@gmail.com>
2016-03-24 14:24:54 -0400

    adding docker host config

---
+++ manifests/docker_host.pp
@@ -0,0 +1,51 @@
+# == profiles::docker_host
+# The basics for systems running containers.
+ class profiles::docker_host(){
+
+  warning("${fqdn} is a puppet managed docker host configured by the profiles::docker_host class")
+  class {'docker':
+    tcp_bind        => 'tcp://0.0.0.0:4243',
+    socket_bind     => 'unix:///var/run/docker.sock',
+  } ->
+
+  file{'/root/dockerhost_remove_images.sh':
+    ensure  => file,
+    mode    => '0777',
+    source => 'puppet:///profiles/files/dockerhost_remove_images.sh',
+  }
+  file{'/root/dockerhost_remove_stale_containers.sh':
+    ensure  => file,
+    mode    => '0777',
+    source => 'puppet://profiles/files/dockerhost_remove_stale_containers.sh',
+  }
+
+  file {'/root/docker_host_join_shipyard.sh':
+    ensure  => file,
+    mode    => '0777',
+    source => 'puppet:///profiles/files/dockerhost_join_shipyard.sh',
+  }
+
+  case $operatingsystem {
+    'Ubuntu':{
+      notice("Docker host ${fqdn} is peparing Ubuntu Based containers")
+      docker::image {'ubuntu':
+        image_tag =>  ['trusty']
+      }
+      docker::image{'msopenstack/sentinel-ubuntu':
+        image_tag =>  ['latest']
+      }
+    }
+    'Centos':{
+      notice("docker host ${fqdn} is peparing Centos Based containers")
+      docker::image{'centos':
+        image_tag =>  ['centos7']
+      }
+      docker::image{'msopenstack/sentinel-centos':
+        image_tag =>  ['latest']
+      }
+    }
+    default:{
+      warning('The Profiles::Docker_Host module doesn't currently support this platform!')
+    }
+  }
+}
