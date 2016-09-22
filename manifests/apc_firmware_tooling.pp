74f6db556ad4b7d9ab99ed0dab01869a6495bf36
primeministerp <primeministerpete@gmail.com>
2016-09-22 12:11:00 -0400

    fixing typo

--- manifests/apc_firmware_tooling.pp
+++ manifests/apc_firmware_tooling.pp
@@ -24,8 +24,8 @@ class profiles::apc_firmware_tooling {
     creates => [
       'C:\Programdata\staging\apc_hw05_aos640_rpdu2g640_bootmon108',
       'C:\Programdata\staging\apc_hw05_aos640_rpdu2g640_bootmon108\pc_hw05_aos_640.bin',
-      'C:\Programdata\staging\apc_hw05_aos640_rpdu2g640_bootmon108\apc_hw05_bootmon_108.bin'
-      'C:\Programdata\staging\apc_hw05_aos640_rpdu2g640_bootmon108\apc_hw05_rpdu2g_640.bin'
+      'C:\Programdata\staging\apc_hw05_aos640_rpdu2g640_bootmon108\apc_hw05_bootmon_108.bin',
+      'C:\Programdata\staging\apc_hw05_aos640_rpdu2g640_bootmon108\apc_hw05_rpdu2g_640.bin',
       'C:\Programdata\staging\apc_hw05_aos640_rpdu2g640_bootmon108\config.txt',
       'C:\Programdata\staging\apc_hw05_aos640_rpdu2g640_bootmon108\FW_Upgrade_R2.exe',
       'C:\Programdata\staging\apc_hw05_aos640_rpdu2g640_bootmon108\iplist.txt',
