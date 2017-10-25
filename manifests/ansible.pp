073d5facc157e955ea094227f21399c3b8ce79be
Peter J. Pouliot <peter@pouliot.net>
2017-10-25 14:58:02 -0400

    class to configure ansible ppa/yum repos

---
+++ manifests/ansible.pp
@@ -0,0 +1,28 @@
+class profiles::ansible {
+  case $::osfamily {
+    'Debian':{
+      include apt
+      apt::ppa{'ppa:ansible/ansible':}
+    } 
+    'Redhat':{
+      case $operatingsystemmajrelease {
+        '6','7','25','26':{
+          yumrepo { "releases-ansible-${operatingsystem}-${operatingsystemmajrelease}":
+            baseurl  => "https://releases.ansible.com/ansible/${operatingsystemmajrelease}",
+            descr    => "Ansible Releases: ${operatingsystem} ${operatingsystemmajrelease} YUM Repository)",
+            enabled  => '0',
+            gpgcheck => '1',
+            gpgkey   => 'http://releases.ansible.com/keys/RPM-GPG-KEY-ansible-release.pub',
+          }
+        }
+        default:{
+          failure("ansible: ${fqdn} is not supported ${operatingsystem} ${operatingsystemmajrelease}  release!")
+        }
+      }
+    }
+    default:{
+      failure("ansible: ${fqdn} does not have ansible release package repository!")
+    }
+  }
+
+}
