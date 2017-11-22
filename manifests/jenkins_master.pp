381c9d532d0b5793233b315f2a8be3812ac4533a
Peter J. Pouliot <peter@pouliot.net>
2017-11-22 09:00:15 -0500

    adding scripts and octokit

--- manifests/jenkins_master.pp
+++ manifests/jenkins_master.pp
@@ -63,8 +63,11 @@ class profiles::jenkins_master () {
     provider        => gem,
   }
 
-  # Puppetlabs Beaker Acceptance Testing Tool
+  # Gem Tools
   package{[
+    # Ruby toolkit for the github api
+    'octokit',
+    # Puppetlabs Beaker Acceptance Testing Tool
     'beaker',
     'beaker-answers',
     'beaker-facter',
@@ -348,4 +351,34 @@ class profiles::jenkins_master () {
   Vcsrepo{
     require => Package['git'],
   }
+    file{'/home/jenkins/bin':
+      ensure  => directory,
+      owner   => 'jenkins',
+      group   => 'jenkins',
+      mode    => '0770',
+      require => User['jenkins'],
+    } -> 
+    file{'/home/jenkins/bin/github_curl_owner_repo_size.sh':
+      ensure  => file,
+      owner   => 'jenkins',
+      group   => 'jenkins',
+      mode    => '0770',
+      content => '#!/usr/bin/env bash
+if [ "$#" -eq 2 ]; then
+    echo "$(echo "scale=2; $(curl https://api.github.com/repos/$1/$2 2>/dev/null \
+    | grep size | head -1 | tr -dc '[:digit:]') / 1024" | bc)MB"
+elif [ "$#" -eq 3 ] && [ "$1" == "-z" ]; then
+    # For some reason Content-Length header is returned only on second try
+    curl -I https://codeload.github.com/$2/$3/zip/master &>/dev/null  
+    echo "$(echo "scale=2; $(curl -I https://codeload.github.com/$2/$3/zip/master \
+    2>/dev/null | grep Content-Length | cut -d' ' -f2 | tr -d '\r') / 1024 / 1024"| bc)MB"
+else
+    printf "Usage: $(basename $0) [-z] OWNER REPO\n\n"
+    printf "Get github repository size or, optionally [-z], the size of the zipped\n"
+    printf "master branch (`Download ZIP` link on repo page).\n"
+    exit 1
+fi
+',
+
+    }
 }
