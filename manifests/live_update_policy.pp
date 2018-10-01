4565a93ff1500126be281cff8e41ad53c98b6545
root <root@puppetmaster.bos1.rakops.com>
2018-10-01 16:22:56 +0000

    updating

--- manifests/live_update_policy.pp
+++ manifests/live_update_policy.pp
@@ -1,11 +1,10 @@
 class profiles::live_update_policy {
-  include apt
+  include ::apt
   if $apt_has_updates == true {
     exec{"apt-dist-upgrade_and_purge_autoremove-${fqdn}":
       command   => '/usr/bin/apt-get update -y && apt-get dist-upgrade -y && apt-get --purge autoremove -y',
       logoutput => true,
       timeout   => '0',
-      require   => Class['apt'],
     }
   }
 }
