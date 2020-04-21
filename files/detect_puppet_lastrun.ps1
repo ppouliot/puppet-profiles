7e566752ed2bb9bbe53dd04b4487d77ba96c0efc
primeministerp <primeministerpete@gmail.com>
2020-04-21 19:37:26 -0400

    trying to get test to run

--- files/detect_puppet_lastrun.ps1
+++ files/detect_puppet_lastrun.ps1
@@ -1,4 +1,4 @@
-$puppet_lastrun = (puppet lastrun)
+$puppet_lastrun = (C:\Program Files\Puppet Labs\Puppet\puppet\bin\puppet lastrun)
 $puppet_lastrun_response = "Error: 'lastrun' has no default action.  See \`puppet help lastrun\`."
 
 If ( $puppet_lastrun -eq $puppet_lastrun_response ){
