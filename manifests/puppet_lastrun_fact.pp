c9c13e7fdae72cea234d272ed0bbc96a3a518f2d
primeministerp <primeministerpete@gmail.com>
2020-04-21 19:00:59 -0400

    updating

--- manifests/puppet_lastrun_fact.pp
+++ manifests/puppet_lastrun_fact.pp
@@ -5,7 +5,26 @@ class profiles::puppet_lastrun_fact {
     'Linux':{
       $local_facter_base_path = '/etc/facter'
       $puppet_command = '/usr/local/bin/puppet'
+      $detect_script = "detect_puppet_lastrun.sh"
+      $detect_script_content = '#!/usr/bin/env bash
+#
+# puppet lastrun detect script (linux)
+#
 
+PUPPET_LASTRUN=$(puppet lastrun)
+PUPPET_LASTRUN_RESPONSE="Error: 'lastrun' has no default action.  See \`puppet help lastrun\`."
+
+# echo $PUPPET_LASTRUN
+# echo $PUPPET_LASTRUN_RESPONSE
+
+if [ "$PUPPET_LASTRUN" == "$PUPPET_LASTRUN_RESPONSE" ]; then
+  echo "Good!"
+  exit 0
+else
+  echo "Bad!"
+  exit 1
+fi
+'
       File{
         owner   => 'root',
         group   => 'root',
@@ -14,6 +33,19 @@ class profiles::puppet_lastrun_fact {
     'windows':{
       $local_facter_base_path = 'C:/ProgramData/PuppetLabs/facter'
       $puppet_command = 'C:/Program Files/Puppet Labs/Puppet/puppet/bin/puppet'
+      $detect_script = "detect_puppet_lastrun.ps1"
+      $detect_script_content = '# puppet lastrun detect script (windows)
+$puppet_lastrun = (puppet lastrun)
+$puppet_lastrun_response = "Error: 'lastrun' has no default action.  See \`puppet help lastrun\`."
+
+If ( "$puppet_lastrun"  -eq "$puppet_lastrun_response" ){
+  echo "Good!"
+  exit 0
+} else {
+  echo "Bad!"
+  exit 1
+}
+'
       File{
         owner   => 'Everyone',
         group   => 'Administrators',
@@ -22,15 +54,32 @@ class profiles::puppet_lastrun_fact {
     default:{
       $local_facter_base_path = "/etc/puppetlabs/facter"
       $puppet_command = '/usr/local/bin/puppet'
+      $detect_script = "${local_facter_base_path}/detect_puppet_lastrun.sh"
+      $detect_script_content = '#!/usr/bin/env bash
+PUPPET_LASTRUN=$(puppet lastrun)
+PUPPET_LASTRUN_RESPONSE="Error: 'lastrun' has no default action.  See \`puppet help lastrun\`."
+
+# echo $PUPPET_LASTRUN
+# echo $PUPPET_LASTRUN_RESPONSE
+
+if [ "$PUPPET_LASTRUN" == "$PUPPET_LASTRUN_RESPONSE" ]; then
+  echo "Good!"
+  exit 0
+else
+  echo "Bad!"
+  exit 1
+fi
+'
       File{
         owner   => 'root',
         group   => 'root',
       }
     }
   }
+
   $local_fact_path = "${local_factor_base_path}/facts.d"
 
-  file{"${local_facter_base_path}/detect_puppet_lastrun.sh":
+  file{"${local_facter_base_path}/${detect_script}":
     ensure  => 'file',
     mode    => '0777',
     source  => 'puppet:///modules/profiles/detect_puppet_lastrun.sh',
@@ -39,7 +88,7 @@ class profiles::puppet_lastrun_fact {
 
   exec{'Generating Puppet LastRUN information':
     command => "${puppet_command} lastrun info | sed \'s/^\ \ \"/\ \ \"lastrun_/g\' > ${local_fact_path}/lastrun.json",
-    onlyif  => "${local_facter_base_path}/detect_puppet_lastrun.sh",
+    onlyif  => "${local_facter_base_path}/${detect_script}",
   }
 
 }
