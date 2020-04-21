b05b6b12f48c23129d194a366f6b618ba98cab36
primeministerp <primeministerpete@gmail.com>
2020-04-21 19:24:13 -0400

    updating scripts

--- files/detect_puppet_lastrun.ps1
+++ files/detect_puppet_lastrun.ps1
@@ -1,13 +1,9 @@
-# puppet lastrun detect script (windows)
-
 $puppet_lastrun = (puppet lastrun)
 $puppet_lastrun_response = "Error: 'lastrun' has no default action.  See \`puppet help lastrun\`."
 
-If ( "$puppet_lastrun"  -eq "$puppet_lastrun_response" ){
-  echo "Good!"
-  exit 0
+If ( $puppet_lastrun -eq $puppet_lastrun_response ){
+  'Good!'
 } else {
-  echo "Bad!"
-  exit 1
+  'Bad!'
 }
 
