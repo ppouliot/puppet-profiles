859c8679fd21ef030732af0612d545efd3c2eb04
primeministerp <primeministerpete@gmail.com>
2020-04-21 13:45:29 -0400

    updating

---
+++ manifests/puppet_binary_path_fact.pp
@@ -0,0 +1,34 @@
+# profiles::puppet_binary_path_fact
+class profiles::puppet_binary_path_fact {
+  case $::kernel {
+    'Linux':{
+      $local_fact_path      = "/etc/facter/facts.d"
+      $local_fact_extension = 'sh',
+      $local_fact_content   = '# Puppet Managed Fact: puppet_binary_path
+#!/usr/bin/env bash
+PUPPET_BINARY_PATH=`which puppet`
+echo "puppet_binary_path=$PUPPET_BINARY_PATH"'
+
+    'windows':{
+      $local_fact_path = "C:/ProgramData/PuppetLabs/facter/facts.d"
+      $local_fact_extension = 'ps1',
+      $local_fact_content = '# Puppet Managed Fact: puppet_binary_path
+$puppet_binary_path = Get-Command puppet | select -expandproperty Path
+Write-Host  "puppet_binary_path=$puppet_binary_path'
+
+    default:{
+      $local_fact_path = "/etc/puppetlabs/facter"
+      $local_fact_extension = 'sh',
+      $local_fact_content   = '# Puppet Managed Fact: puppet_binary_path
+#!/usr/bin/env bash
+PUPPET_BINARY_PATH=`which puppet`
+echo "puppet_binary_path=$PUPPET_BINARY_PATH"'
+    }
+  }
+  # Create a Fact to detect the puppet binary path.  This will assist on the first puppet run.
+  file{'puppet_binary_path':
+    path    => "${local_fact_path}/puppet_binary_path.${local_fact_extesion}",
+    ensure  => 'file',
+    content => $local_fact_content,
+  }
+}
--- manifests/puppet_lastrun_fact.pp
+++ manifests/puppet_lastrun_fact.pp
@@ -1,7 +1,21 @@
 # profiles::puppet_lastrun_fact
 class profiles::puppet_lastrun_fact {
 
-  file{'/etc/puppetlabs/facter/detect_puppet_lastrun.sh':
+  case $::kernel {
+    'Linux':{
+      $local_facter_base_path = "/etc/facter"
+    }
+
+    'windows':{
+      $local_facter_base_path = "C:/ProgramData/PuppetLabs/facter"
+    }
+    default:{
+      $local_facter_base_path = "/etc/puppetlabs/facter"
+    }
+  }
+  $local_fact_path = "${local_factor_base_path}/facts.d"
+
+  file{"${local_facter_base_path}/detect_puppet_lastrun.sh":
     ensure  => 'file',
     owner   => 'root',
     group   => 'root',
@@ -11,7 +25,7 @@ class profiles::puppet_lastrun_fact {
   } ->
 
   exec{'Generating Puppet LastRUN information':
-    command => '/opt/puppetlabs/bin/puppet lastrun info | sed \'s/^\ \ "/\ \ "lastrun_/g\' > /etc/puppetlabs/facter/facts.d/lastrun.json',
+    command => "${puppet_binary_path} lastrun info | sed \'s/^\ \ "/\ \ "lastrun_/g\' > ${local_fact_path}/lastrun.json',
     onlyif  => '/etc/puppetlabs/facter/detect_puppet_lastrun.sh',
   }
 
