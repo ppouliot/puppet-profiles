6d2506433963aa5f4d40f367176e646bd746c1b0
primeministerp <primeministerpete@gmail.com>
2020-04-21 21:42:27 -0400

    working windows script

--- files/detect_puppet_lastrun.ps1
+++ files/detect_puppet_lastrun.ps1
@@ -1,9 +1,3 @@
-$puppet_lastrun = (C:\Program Files\Puppet Labs\Puppet\puppet\bin\puppet lastrun)
-$puppet_lastrun_response = "Error: 'lastrun' has no default action.  See \`puppet help lastrun\`."
-
-If ( $puppet_lastrun -eq $puppet_lastrun_response ){
-  'Good!'
-} else {
-  'Bad!'
-}
-
+Start-Process -FilePath 'C:\Program Files\Puppet Labs\Puppet\bin\puppet.bat' -ArgumentList 'lastrun' -NoNewWindow -Wait | Tee-Object -Variable puppet_lastrun
+$puppet_lastrun_response = "Error: 'lastrun' has no default action.  See ``puppet help lastrun``. "
+$puppet_lastrun -eq (Write-Host $puppet_lastrun_response -ForegroundColor Red)
