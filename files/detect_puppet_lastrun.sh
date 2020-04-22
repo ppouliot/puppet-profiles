ac709c027e148ee38e527f1ad3139488856c0543
primeministerp <primeministerpete@gmail.com>
2020-04-22 07:43:06 -0400

    fixing script

--- files/detect_puppet_lastrun.sh
+++ files/detect_puppet_lastrun.sh
@@ -2,13 +2,13 @@
 
 # puppet lastrun detect script (linux)
 
-PUPPET_LASTRUN=$(puppet lastrun)
-PUPPET_LASTRUN_RESPONSE="Error: 'lastrun' has no default action.  See \`puppet help lastrun\`."
+PUPPET_LASTRUN="$(puppet lastrun --color=false 2>&1)"
+PUPPET_LASTRUN_RESPONSE='Error: '\''lastrun'\'' has no default action.  See '\`'puppet help lastrun'\`'.'
 
-# echo $PUPPET_LASTRUN
-# echo $PUPPET_LASTRUN_RESPONSE
+echo "${PUPPET_LASTRUN}"
+echo "${PUPPET_LASTRUN_RESPONSE}"
 
-if [ "$PUPPET_LASTRUN" == "$PUPPET_LASTRUN_RESPONSE" ]; then
+if [[ "${PUPPET_LASTRUN}" == "${PUPPET_LASTRUN_RESPONSE}" ]]; then
   echo "Good!"
   exit 0
 else
